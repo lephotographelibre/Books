19b13ea8d3ad55be6033fb95571bb4cda2926563
lephotographelibre <lephotographelibre@yahoo.com>
2022-04-05 14:57:22 +0200

    cleanup

--- TestPILresize800px.py
+++ TestPILresize800px.py
@@ -5,14 +5,16 @@ import sys
 def main():
     img = Image.open("plage.jpg")
     print(img.format, img.size, img.mode)
-    img.save('newcoverx800px.jpg')
 
-    img = resize_image(img,800)
-    print(img.format, img.size, img.mode)
-    img.show()
+    img2 = resize_image(img, 800)
+
+    img2.save('newcoverx800px.jpg', 'JPEG')
+    print(img2.format, img2.size, img2.mode)
+    img2.show()
 
 
-'''This script will resize an image (somepic.jpg) using PIL (Python Imaging Library) to a width of 300 pixels and a 
+'''
+This script will resize an image (somepic.jpg) using PIL (Python Imaging Library) to a width of 300 pixels and a 
 height proportional to the new width. It does this by determining what percentage 300 pixels is of the original width 
 (img.size[0]) and then multiplying the original height (img.size[1]) by that percentage. 
 Change "basewidth" to any other number to change the default width of your images.
@@ -22,7 +24,7 @@ Change "basewidth" to any other number to change the default width of your image
 def resize_image(img_input, basewidth):
     wpercent = (basewidth / float(img_input.size[0]))
     hsize = int((float(img_input.size[1]) * float(wpercent)))
-    img = img_input.resize((basewidth, hsize), Image.ANTIALIAS)
+    img = img_input.resize((basewidth, hsize), Image.LANCZOS)
     return img
 
 
