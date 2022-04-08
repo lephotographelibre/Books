f3649a8bfe32de832ff82da8a4366b5600d37d7d
lephotographelibre <lephotographelibre@yahoo.com>
2022-04-08 09:06:47 +0200

    Initial release

---
+++ epublight.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#   Use this script to activate Books Python 3.6 venv
+source /ssdhome/jm/PycharmProjects/Books/bin/activate
+python /ssdhome/jm/PycharmProjects/Books/epublight.py $1
