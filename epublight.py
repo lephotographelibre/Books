5d2ce5d7a4a363d9c1e7f7b10bdc6859c474326c
lephotographelibre <lephotographelibre@yahoo.com>
2022-04-11 18:20:37 +0200

    Correct Github issue #2 add cover detection code

--- epublight.py
+++ epublight.py
@@ -10,13 +10,24 @@
 
 #
 import argparse
-import logging
-import sys
-import os
-import mimetypes
-import zipfile
+import base64
 import io
+import logging
+import mimetypes
+import os
+import os.path
+import sys
+import zipfile
+from xml.dom import minidom
+
 from PIL import Image
+
+IS_PY2 = sys.version_info < (3, 0)
+
+if IS_PY2:
+    from urllib import unquote
+else:
+    from urllib.parse import unquote
 
 
 def main():
@@ -26,6 +37,7 @@ def main():
     parser.add_argument('-l', '--log-level')
     parser.add_argument('--jpeg-quality', type=int, default=50)
     parser.add_argument('--image-resize-percent', type=int)
+    parser.add_argument('--image-resize-resample')
 
     args = parser.parse_args()
 
@@ -64,6 +76,11 @@ def main():
         with zipfile.ZipFile(args.out_epub_filepath, 'w') as out_book:
             for name in in_book.namelist():
                 logging.debug('-- file name -- ' + name)
+                # cover detection
+                is_cover = False
+                if name == _locate_cover(args.in_epub_filepath):
+                    is_cover = True
+                    logging.debug('-- cover found --------------' + name + ' ... isCover = ' + str(is_cover))
                 _files_number = _files_number + 1
                 with in_book.open(name, 'r') as in_file:
                     content = in_file.read()
@@ -92,12 +109,19 @@ def main():
                     else:
                         _font_number = _font_number + 1
 
+    in_book.close()
+    out_book.close()
+
+
+    # Display Stats
     logging.info('-- Number of files in the initial file = ' + str(_files_number))
     logging.info('-- Number of images in the initial file = ' + str(_images_number))
+    logging.info('-- JPEG Quality % = ' + str(args.jpeg_quality))
     if _font_number > 0:
         logging.info('-- Number of font files removed =  = ' + str(_font_number))
     logging.info('-- Initial size = ' + str(os.path.getsize(args.in_epub_filepath)))
     logging.info('-- Final size = ' + str(os.path.getsize(args.out_epub_filepath)))
+
     ratio = os.path.getsize(args.out_epub_filepath) / os.path.getsize(args.in_epub_filepath) * 100
     logging.info('-- Compression Ratio = ' + str(100 - round(ratio)) + '%')
 
@@ -113,7 +137,7 @@ def is_an_egg(name, old_content, args):
     else:
         if len(old_content) == 0:
             # github Issue 1: don't remove empty directories  (content size=0)
-            logging.debug('!!! NOT AN EGG  -----> Name = ' + name + ' Size = ' + str(len(old_content)))
+            logging.debug('!!! NOT AN EGG -- DIRECTORY -----> Name = ' + name + ' Size = ' + str(len(old_content)))
             return old_content
         else:
             # Egg detected
@@ -189,12 +213,124 @@ def compress_image(subtype, old_content, args):
 
 def resize_image(img_input, basewidth):
     """
-    This script will resize an image (img_input) using PIL to a width  and a height proportional to basewidth
+    This script will resize an image (some-pic.jpg) using PIL to a width   and a height proportional to the new width
+    Param: img_input IMAGE, basewidth int
+    Return: IMAGE
     """
     wpercent = (basewidth / float(img_input.size[0]))
     hsize = int((float(img_input.size[1]) * float(wpercent)))
     img = img_input.resize((basewidth, hsize), Image.LANCZOS)
     return img
+
+
+class EPubException(Exception):
+    pass
+
+
+def _locate_cover(epub_filepath):
+    '''
+    References: http://idpf.org/epub/201 and http://idpf.org/epub/301
+    1. Parse META-INF/container.xml file and find the .OPF file path.
+    2. In the .OPF file, find the metadata
+    return cover_image_filepath
+    '''
+    if not zipfile.is_zipfile(epub_filepath):
+        raise EPubException('Unknown file')
+
+    try:
+        # print('Reading ePub file: {}'.format(filepath))
+        zf = zipfile.ZipFile(epub_filepath, 'r', compression=zipfile.ZIP_DEFLATED, allowZip64=True)
+        container = zf.read('META-INF/container.xml')
+        container_xmldoc = minidom.parseString(container)
+        # e.g.: <rootfile full-path="content.opf" media-type="application/oebps-package+xml"/>
+        opf_filepath = container_xmldoc.getElementsByTagName('rootfile')[0].attributes['full-path'].value
+
+        opf = zf.read(os.path.normpath(opf_filepath))
+        opf_xmldoc = minidom.parseString(opf)
+    except IndexError:
+        raise EPubException("Cannot parse raw metadata from {}".format(
+            os.path.basename(epub_filepath)))
+
+    cover_image_content, cover_image_extension, cover_image_filepath = _discover_cover_image(zf, opf_xmldoc, opf_filepath)
+    return cover_image_filepath
+
+    # print("-- cover_image_content = " + str(cover_image_content))
+    # print("-- cover_image_extension = " + cover_image_extension)
+    # print("-- cover_image__filepath = " + cover_image_filepath)
+
+    # in_buffer = io.BytesIO(cover_image_content)
+    # img = Image.open(in_buffer)
+
+
+def resize_image(img_input, basewidth):
+    """
+    This script will resize an image (some-pic.jpg) using PIL to a width   and a height proportional to the new width
+    Param: img_input IMAGE, basewidth int
+    Return: IMAGE
+    """
+    wpercent = (basewidth / float(img_input.size[0]))
+    hsize = int((float(img_input.size[1]) * float(wpercent)))
+    img = img_input.resize((basewidth, hsize), Image.LANCZOS)
+    return img
+
+
+def _discover_cover_image(zf, opf_xmldoc, opf_filepath):
+    '''
+    Find the cover image path in the OPF file.
+    Returns a tuple: (image content in base64, file extension)
+    '''
+    content = None
+    filepath = None
+    extension = None
+
+    # Strategies to discover the cover-image path:
+
+    # e.g.: <meta name="cover" content="cover"/>
+    tag = find_tag(opf_xmldoc, 'meta', 'name', 'cover')
+    if tag and 'content' in tag.attributes.keys():
+        item_id = tag.attributes['content'].value
+        if item_id:
+            # e.g.: <item href="cover.jpg" id="cover" media-type="image/jpeg"/>
+            filepath, extension = find_img_tag(opf_xmldoc, 'item', 'id', item_id)
+    if not filepath:
+        filepath, extension = find_img_tag(opf_xmldoc, 'item', 'id', 'cover-image')
+    if not filepath:
+        filepath, extension = find_img_tag(opf_xmldoc, 'item', 'id', 'cover')
+
+    # If we have found the cover image path:
+    if filepath:
+        # The cover image path is relative to the OPF file
+        base_dir = os.path.dirname(opf_filepath)
+        # Also, normalize the path (ie opfpath/../cover.jpg -> cover.jpg)
+        coverpath = os.path.normpath(os.path.join(base_dir, filepath))
+        try:
+            content = zf.read(coverpath)
+        except KeyError:
+            raise EPubException("Cannot read {} from EPub file {}".format(
+                coverpath, os.path.basename(zf.filename)))
+        content = base64.b64encode(content)
+
+    return content, extension, filepath
+
+
+def find_img_tag(xmldoc, tag_name, attr, value):
+    # print('Finding img tag: <{} {}="{}">'.format(tag_name, attr, value))
+    for tag in xmldoc.getElementsByTagName(tag_name):
+        if attr in tag.attributes.keys() and tag.attributes[attr].value == value:
+            if 'href' in tag.attributes.keys():
+                filepath = unquote(tag.attributes['href'].value)
+                filename, file_extension = os.path.splitext(filepath)
+                if file_extension in ('.gif', '.jpg', '.jpeg', '.png', '.svg'):
+                    return filepath, file_extension
+    return None, None
+
+
+def find_tag(xmldoc, tag_name, attr, value):
+    # print('Finding tag: <{} {}="{}">'.format(tag_name, attr, value))
+    for tag in xmldoc.getElementsByTagName(tag_name):
+        if attr in tag.attributes.keys() and tag.attributes[attr].value == value:
+            return tag
+
 
 
 if __name__ == "__main__":
